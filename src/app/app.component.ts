55ea088f52f98b9efc22d5819fe7aa566167e03f
Kheronn <kheronn@gmail.com>
2019-03-15 17:02:28 -0300

    Remove blank spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 export interface Character {
   name: string;
